57617d7c66106ef8ba4e9071861b645c1a1f2830
jialong.wang5191 <jialong.wang@newtouch.cn>
2016-05-12 17:52:53 +0800

    Update Keypad

--- junit/com/codewars/kata/keypad/KeypadTest.java
+++ junit/com/codewars/kata/keypad/KeypadTest.java
@@ -7,7 +7,13 @@ public class KeypadTest {
 
 	@Test
 	public void simpleTest() {
-		assertEquals(9, Keypad.presses("LOL"));
+//		assertEquals(9, Keypad.presses("LOL"));
 		assertEquals(13, Keypad.presses("HOW R U"));
+		
+		assertEquals(14, Keypad.presses("wbo;DM,xUB"));
+		
+//		assertEquals(19, Keypad.presses("1234213423894280312"));
+		
+		assertEquals(47, Keypad.presses("WHERE DO U WANT 2 MEET L8R"));
 	}
 }
--- src/com/codewars/kata/keypad/Keypad.java
+++ src/com/codewars/kata/keypad/Keypad.java
@@ -12,6 +12,34 @@ public class Keypad {
 	}
 	
 	private static void init() {
+		keyPressNum.put(' ', 1);
+		keyPressNum.put('*', 1);
+		keyPressNum.put('#', 1);
+		
+		for (int c = '0'; c <= '9'; c++) {
+			keyPressNum.put((char) c, 4);
+		}
+
+		keyPressNum.put('0', 2);
+		keyPressNum.put('1', 1);
+		keyPressNum.put('7', 5);
+		keyPressNum.put('9', 5);
+		
+		for (int c = 'a'; c < 'z'; c++) {
+			int pressTimes = 0;
+			if (c == 's') {
+				continue;
+			} else if (c > 's') {
+				pressTimes = ((c - 97) - 1) % 3 + 1;
+			} else {
+				pressTimes = (c - 97) % 3 + 1;
+			}
+			
+			keyPressNum.put((char) c, pressTimes);
+		}
+		
+		keyPressNum.put('s', 4);
+		keyPressNum.put('z', 4);
 	}
 	
 	public static int presses(String phrase) {
@@ -31,8 +59,12 @@ public class Keypad {
 		return count;
 	}
 
-	private static int getCount(char c) {
-		// TODO Auto-generated method stub
-		return 0;
+	private static int getCount(char ch) {
+		char lowcase = Character.toLowerCase(ch);
+		Integer result = keyPressNum.get(lowcase);
+		if (result == null) {
+			return 0;
+		}
+		return result; 
 	}
 }
